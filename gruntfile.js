efa286320b5495d59e578c7a018a4a926a5d44f9
mailtoak82 <mailtoak82@gmail.com>
2015-02-24 17:10:43 +0530

    Added Jade file and compiled

--- gruntfile.js
+++ gruntfile.js
@@ -3,34 +3,32 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
-  htmlhint: {
-    build: {
-        options: {
-            'tag-pair': true,
-            'tagname-lowercase': true,
-            'attr-lowercase': true,
-            'attr-value-double-quotes': true,
-            'doctype-first': true,
-            'spec-char-escape': true,
-            'id-unique': true,
-            'head-script-disabled': true,
-            'style-disabled': true
-        },
-        src: ['index.html']
+ watch: {
+    html: {
+        files: ['main.jade'],
+        tasks: ['jade']
     }
 },
 
-  watch: {
-    html: {
-        files: ['index.html'],
-        tasks: ['htmlhint']
-    }
-}
+jade: {
+          reportal: {
+            options: {
+              pretty: true,
+              data: {
+                debug: true,
+                timestamp: "<%= new Date().getTime() %>"
+              }
+            },
+            files: {
+                "Compiled/main.html": "main.jade"
+              }
+          }
+        }
 
 })
     
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
